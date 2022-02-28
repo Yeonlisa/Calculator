45ca6b69313258abd5d679360251fe2cab9952fb
yeonlisa <tkzw4869@gmail.com>
2022-03-01 00:34:40 +0900

    Build Calculaor with Jest tests

---
+++ src/modules/cal.test.ts
@@ -0,0 +1,62 @@
+import Calc, { CalcInput, InputType, Operation, OperatorType } from "./calc";
+
+test('generates operations', () => {
+    const inputs: Array<CalcInput> = [
+        {type: InputType.Numerical, value: 1},
+        {type: InputType.Numerical, value: 2},
+        {type: InputType.Operator, operator: OperatorType.Add},
+        {type: InputType.Numerical, value: 3},
+        {type: InputType.Operator, operator: OperatorType.Equals},
+    ]
+
+    const operations: Array<Operation> = [
+        { operator: OperatorType.Add, value: 12 },
+        { operator: OperatorType.Add, value: 3 },
+        { operator: OperatorType.Equals, value: 0 },
+    ];
+    
+    expect(Calc.getOperationsBuilder(inputs).operations).toEqual(operations);
+});
+
+test('has displayValue of 0 with no input', () => {
+    const inputs: Array<CalcInput> = [];
+    const state = Calc.getState(inputs);
+    expect(state.displayValue).toEqual(0);
+});
+
+test('derives displayValue upon new numerical input', () => {
+    const inputs: Array<CalcInput> = [
+        {type: InputType.Numerical, value: 1},
+        {type: InputType.Numerical, value: 2},
+        {type: InputType.Operator, operator: OperatorType.Add},
+        {type: InputType.Numerical, value: 3},
+    ]
+    const state = Calc.getState(inputs);
+    expect(state.displayValue).toEqual(3);
+});
+
+test('derive final state', () => {
+    const inputs: Array<CalcInput> = [
+        {type: InputType.Numerical, value: 1},
+        {type: InputType.Numerical, value: 2},
+        {type: InputType.Operator, operator: OperatorType.Add},
+        {type: InputType.Numerical, value: 3},
+        {type: InputType.Operator, operator: OperatorType.Equals},
+    ]
+    const state = Calc.getState(inputs);
+    expect(state.displayValue).toEqual(15);
+});
+
+test('derive final state (with addition and subtraction)', () => {
+    const inputs: Array<CalcInput> = [
+        {type: InputType.Numerical, value: 1},
+        {type: InputType.Numerical, value: 2},
+        {type: InputType.Operator, operator: OperatorType.Add},
+        {type: InputType.Numerical, value: 3},
+        {type: InputType.Operator, operator: OperatorType.Subtract},
+        {type: InputType.Numerical, value: 5},
+        {type: InputType.Operator, operator: OperatorType.Equals},
+    ]
+    const state = Calc.getState(inputs);
+    expect(state.displayValue).toEqual(10);
+});
